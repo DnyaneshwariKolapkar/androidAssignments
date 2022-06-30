e57d1381875da66954e9f54c2baee8552d3403e5
DnyaneshwariKolapkar <kolapkardnyaneshwari@gmail.com>
2022-06-30 18:58:24 +0530

    added greet function

--- app/src/main/java/com/example/allassignments/MainActivity.kt
+++ app/src/main/java/com/example/allassignments/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.allassignments
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn1 = findViewById<Button>(R.id.button1)
+
+        btn1.setOnClickListener {
+            startActivity(Intent(this, bdaygreet::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/allassignments/bdaygreet.kt
@@ -0,0 +1,19 @@
+package com.example.allassignments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class bdaygreet : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bdaygreet)
+        val btn = findViewById<Button>(R.id.wishbutton)
+        val name = findViewById<EditText>(R.id.editTextTextPersonName)
+        btn.setOnClickListener {
+            Toast.makeText(this, "Hello "+ name.text + "!!!", Toast.LENGTH_LONG).show()
+        }
+    }
+}//
